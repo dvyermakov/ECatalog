ab19a4ed891f51c4cb5dd7705067b5d6f1f2e12f
dmitriy <d.v.yermakov@gmail.com>
2022-03-28 22:23:56 +0300

    Добавлен файл с параметрами доступа

---
+++ inc/connection.h
@@ -0,0 +1,9 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#define EXAMPLE_HOST "192.168.1.155"
+#define EXAMPLE_USER "dmitriy"
+#define EXAMPLE_PASS "R3h7sow1."
+#define EXAMPLE_DB "ECatalog"
+
+#endif
